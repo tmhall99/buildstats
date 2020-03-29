67f2846a38582885a786d2ce92914c9871e0df8c
Tom Hall <thomas.hall@silvercar.com>
2020-03-29 14:32:20 -0500

    add duration metric

--- buildstats/main.py
+++ buildstats/main.py
@@ -1,20 +1,45 @@
 import requests
 import os
 from requests.utils import default_user_agent
+import pprint
 
 
 def main():
-    stats = get_credits_used(os.environ.get("CIRCLE_TOKEN"), "gh/silvercar/mob-api")
-    print(stats["text"])
-    print(
-        "\n".join(
-            "{:<24}\t{:>10}".format(k, v) for k, v in stats["credits_used"].items()
+    pprint.pprint(get_jobs(os.environ.get("CIRCLE_TOKEN"), "gh/silvercar/mob-api"))
+    stats = get_credits_by_job(os.environ.get("CIRCLE_TOKEN"), "gh/silvercar/mob-api")
+    print("\n".join("{:<24}\t{:>10}".format(k, v) for k, v in stats.items()))
+    print("\n")
+    stats = get_duration_by_job(os.environ.get("CIRCLE_TOKEN"), "gh/silvercar/mob-api")
+    print("\n".join("{:<24}\t{:>10}".format(k, v) for k, v in stats.items()))
+
+
+def get_duration_by_job(circle_token, project_slug):
+    jobs = get_jobs(circle_token, project_slug)
+
+    return {
+        job["name"]: job["metrics"]["duration_metrics"]["p95"]
+        for job in sorted(
+            jobs["items"],
+            key=lambda job: job["metrics"]["duration_metrics"]["p95"],
+            reverse=True,
         )
-    )
+    }
 
 
-def get_credits_used(circle_token, project_slug):
-    result = dict()
+def get_credits_by_job(circle_token, project_slug):
+    jobs = get_jobs(circle_token, project_slug)
+
+    return {
+        job["name"]: job["metrics"]["total_credits_used"]
+        for job in sorted(
+            jobs["items"],
+            key=lambda job: job["metrics"]["total_credits_used"],
+            reverse=True,
+        )
+    }
+
+
+def get_jobs(circle_token, project_slug):
     r = requests.get(
         "https://circleci.com/api/v2/insights/{0}/workflows/build/jobs".format(
             project_slug
@@ -24,22 +49,14 @@ def get_credits_used(circle_token, project_slug):
             "User-Agent": "{0} {1}".format("tmhall99/buildstats", default_user_agent()),
         },
     )
-    result["text"] = r.text
+    r.raise_for_status()
     jobs = r.json()
 
     if jobs["next_page_token"] is not None:
         print("Too many jobs")
         return
 
-    result["credits_used"] = {
-        job["name"]: job["metrics"]["total_credits_used"]
-        for job in sorted(
-            jobs["items"],
-            key=lambda job: job["metrics"]["total_credits_used"],
-            reverse=True,
-        )
-    }
-    return result
+    return jobs
 
 
 if __name__ == "__main__":
