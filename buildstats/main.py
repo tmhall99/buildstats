3a990216dc559eae4a26f8df8ce40ac11ef4940d
Tom Hall <thomas.hall@silvercar.com>
2020-03-28 22:01:02 -0500

    pretty cli output

--- buildstats/main.py
+++ buildstats/main.py
@@ -3,18 +3,29 @@ import os
 
 
 def main():
+    stats = getstats(os.environ.get("CIRCLE_TOKEN"))
+    print(stats["text"])
+    print(
+        "\n".join(
+            "{:<24}\t{:>10}".format(k, v) for k, v in stats["credits_used"].items()
+        )
+    )
+
+
+def getstats(circle_token):
+    result = dict()
     r = requests.get(
         "https://circleci.com/api/v2/insights/gh/silvercar/mob-api/workflows/build/jobs",
-        headers={"Circle-Token": os.environ.get("CIRCLE_TOKEN")},
+        headers={"Circle-Token": circle_token},
     )
-    print(r.text)
+    result["text"] = r.text
     jobs = r.json()
 
     if jobs["next_page_token"] is not None:
         print("Too many jobs")
         return
 
-    s = {
+    result["credits_used"] = {
         job["name"]: job["metrics"]["total_credits_used"]
         for job in sorted(
             jobs["items"],
@@ -22,7 +33,7 @@ def main():
             reverse=True,
         )
     }
-    print(s)
+    return result
 
 
 if __name__ == "__main__":
